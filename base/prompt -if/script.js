583206b7d404474217ae3e4008d4ba3c138b7f7e
Karolina Dys <karolinadyss@gmail.com>
2020-07-09 11:53:00 +0200

    add if prompt

---
+++ base/prompt -if/script.js
@@ -0,0 +1,10 @@
+(function() {
+    const cake = prompt("Want some cake?", "yes" , "no");
+
+    if (cake === "yes") {
+        alert("Congratulation ");
+    } else (cake === "no") 
+        alert( "More for me");
+    }
+    
+  )();
