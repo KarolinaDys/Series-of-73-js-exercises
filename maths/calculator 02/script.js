8f11a5fb3d621f217fffc7dd6b61058690f72b8f
Karolina Dys <karolinadyss@gmail.com>
2020-07-13 09:48:16 +0200

    fixed calculator two

--- maths/calculator 02/script.js
+++ maths/calculator 02/script.js
@@ -1,16 +1,30 @@
 (() => {
     // to get the value of an input: document.getElementById("element-id").value
-    let opOne = Number(document.getElementById("op-one").value);
-    let  opTwo = Number(document.getElementById("op-two").value);
+    let number_1 = document.getElementById('op-one');
+    let number_2 = document.getElementById('op-two');
+
     const performOperation = operation => {
         // perform the operation
-        addition = opOne + opTwo;
-        substraction = opOne - opTwo;
-        multiplication = opOne * opTwo;
-        division = opOne / opTwo;
-
+        let total = 0;
+        switch (operation) {
+            case 'addition':
+                total = parseInt(number_1.value) + parseInt(number_2.value);
+                break;
+            case 'substraction':
+                total = parseInt(number_1.value) - parseInt(number_2.value);
+                break;
+            case 'multiplication':
+                total = parseInt(number_1.value) * parseInt(number_2.value);
+                break;
+            case 'division':
+                total = parseInt(number_1.value) / parseInt(number_2.value);
+                total = Math.round((total + Number.EPSILON) * 100) / 100;
+                break;
+            default:
+            // code block
+        }
+        alert(total);
     };
-
     Array.from(document.querySelectorAll("button.operator")).forEach($btn =>
         $btn.addEventListener(
             "click",
