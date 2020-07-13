ce0d61473f514134bf8bdac1fd9c253ddf4e661a
Karolina Dys <karolinadyss@gmail.com>
2020-07-13 15:27:34 +0200

    add bcg one

---
+++ colors/change-bcg 0ne/script.js
@@ -0,0 +1,15 @@
+(function() {
+    document.getElementById("red").addEventListener('click', function(){
+        document.body.style.background = "red";
+    });
+    document.getElementById("green").addEventListener('click', function(){
+        document.body.style.background = "green";
+    });
+    document.getElementById("yellow").addEventListener('click', function(){
+        document.body.style.background = "yellow";
+    });
+    document.getElementById("blue").addEventListener('click', function(){
+        document.body.style.background = "blue";
+    });
+
+})();
