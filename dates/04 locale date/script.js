c303da04394e16a0f67d5d2e715a321fb7a018a6
Karolina Dys <karolinadyss@gmail.com>
2020-07-14 16:57:40 +0200

    add names months

--- dates/04 locale date/script.js
+++ dates/04 locale date/script.js
@@ -5,8 +5,9 @@
     // your code here
     const now = new Date();
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thersday", "Friday", "Saturday"];
+    const months =["January" , "February" , "March", "April" , "May", "June", "July", "Augustus", "September" , "October" ,"November", "Decembe"] 
     document.getElementById("target").innerHTML = days[now.getDay()] + " " + now.getDate()+ "/" +
-     (now.getMonth()+1) + "/" + now.getFullYear() + " , " + now.getHours() + "h" + now.getMinutes() + "."+ now.getSeconds()+ "sec";
+     months[now.getMonth()] + "/" + now.getFullYear() + " , " + now.getHours() + "h" + now.getMinutes() + "."+ now.getSeconds()+ "sec";
 
 
 })();
