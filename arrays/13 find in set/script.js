7c14dd9d30faf2e056690f5ae56cad376702d725
Karolina Dys <karolinadyss@gmail.com>
2020-07-17 14:26:58 +0200

    add find in set

--- arrays/13 find in set/script.js
+++ arrays/13 find in set/script.js
@@ -12,4 +12,16 @@
     ]);
 
     // your code here
+document.getElementById("run").addEventListener("click" , function(){
+    console.log(people.size)
+    if(people.has("Alexandre") == true ){
+    console.log("Alexandre is on the list")
+}
+    else{
+        console.log("Alexandre isn't on the list")
+    }
+    
+
+})
+   
 })();
