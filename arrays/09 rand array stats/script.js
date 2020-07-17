5e58ca888aef4a3c2af51b90046bbf53c381b868
Karolina Dys <karolinadyss@gmail.com>
2020-07-17 09:23:59 +0200

    add rand arrays stats

--- arrays/09 rand array stats/script.js
+++ arrays/09 rand array stats/script.js
@@ -1,5 +1,39 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click",function(){
+        function number(min, max) {
+            min = 1;
+            max = 100;
+            return (Math.random() * (max - min)) + min;
+             
+        }
+    const tab =[
+        document.getElementById("n-1").innerHTML = Math.round(number()),
+        document.getElementById("n-2").innerHTML = Math.round(number()),
+        document.getElementById("n-3").innerHTML = Math.round(number()),
+        document.getElementById("n-4").innerHTML = Math.round(number()),
+        document.getElementById("n-5").innerHTML = Math.round(number()),
+        document.getElementById("n-6").innerHTML = Math.round(number()),
+        document.getElementById("n-7").innerHTML = Math.round(number()),
+        document.getElementById("n-8").innerHTML = Math.round(number()),
+        document.getElementById("n-9").innerHTML = Math.round(number()),
+        document.getElementById("n-10").innerHTML = Math.round(number()),
+    ]
+    function compareNr(a, b) {
+        return a - b
+    }
+    document.getElementById("min").innerHTML = tab.sort(compareNr)[0];
+    document.getElementById("max").innerHTML = tab.sort(compareNr)[9];
+          let sum = 0;
+          for (i = 0; i < tab.length; i++) {
+            sum = sum + tab[i];
+    
+          }
+          document.getElementById("sum").innerHTML = sum;    
+          let result = 0;
+           result = sum / tab.length
+         document.getElementById("average").innerHTML = result;
 
+    })    
 })();
