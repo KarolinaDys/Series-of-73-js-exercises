a6552d797c3bd89d9e22f85793f8599dfb3fe3f0
Karolina Dys <karolinadyss@gmail.com>
2020-07-22 11:28:29 +0200

    add waving text

---
+++ misc/01-waving-text/script.js
@@ -0,0 +1,6 @@
+(function() {
+
+    // your code here
+ document.getElementById("target").innerHTML ="Woo-hoo!".fontsize(2) + "I'm making waves!".fontsize(3)+"Wéééé!".fontsize(4)
+  +"Oh ...".fontsize(5)+ "I think I'm having seasickness ...".fontsize(6)
+})();
