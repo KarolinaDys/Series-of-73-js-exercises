a570eb4ba42090e84551cb5a542832c389b4a711
Karolina Dys <karolinadyss@gmail.com>
2020-07-27 14:35:58 +0200

    add typewriter effect

--- misc/02-typewriter-effect/script.js
+++ misc/02-typewriter-effect/script.js
@@ -1,5 +1,26 @@
 (function() {
 
-    // your code here
+    function one (x) {
+        return document.getElementById(x);
+    }
+
+    var textWrapper = one("target");
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    for (i = 0; i < document.getElementsByClassName("letter").length; i++) {
+        document.getElementsByClassName("letter")[i].style = "opacity:0"
+    }
+
+    let time = 100
+    let a = 0
+    iterate();
+    function iterate() {
+        document.getElementsByClassName("letter")[a].style = "opacity:1", time
+        a += 1;
+        time = 50 + Math.pow(Math.random()*1,2) ;
+        if (a < document.getElementsByClassName("letter").length) {
+            setTimeout(iterate, time)
+        };
+    }
 
 })();
