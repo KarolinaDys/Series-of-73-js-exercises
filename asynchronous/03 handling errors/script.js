41fa984963ce4378e20c407114b46829c4ab2dfb
Karolina Dys <karolinadyss@gmail.com>
2020-07-27 14:13:45 +0200

    add handling errors

--- asynchronous/03 handling errors/script.js
+++ asynchronous/03 handling errors/script.js
@@ -1,3 +1,12 @@
 (() => {
-    // your code here
+    function myCallbackFuntion(error, persons) {
+        persons.forEach(element => {
+            console.log(element.id);
+        });
+    } 
+
+    document.getElementById("run").onclick = () => {
+       console.error( window.lib.getPersons(myCallbackFuntion));
+        
+    };
 })();
