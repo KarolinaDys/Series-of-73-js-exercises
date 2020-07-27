59f7fc5c7f8963ddfe8e5a57189ab18988a1370a
Karolina Dys <karolinadyss@gmail.com>
2020-07-27 15:49:38 +0200

    wrost ui two

--- misc/04-worst-ui-two/script.js
+++ misc/04-worst-ui-two/script.js
@@ -1,5 +1,36 @@
 (function() {
 
     // your code here
+    
+function update(){
+document.getElementById('target').innerHTML = 
+document.getElementById('part-one').innerHTML 
++ document.getElementById('part-two').innerHTML 
++ document.getElementById('part-three').innerHTML 
++ document.getElementById('part-four').innerHTML
+};
+let x = 0;
+let min = 0;
+let max = 9;
+x >= min;
+x <= max;
+document.getElementById('part-one').addEventListener("click", function(){
+   document.getElementById('part-one').innerHTML = (Number(document.getElementById('part-one').innerHTML)+ Math.round((00) + 1));
+   update();
+}) ;
+
+document.getElementById('part-two').addEventListener("click", function(){
+    document.getElementById('part-two').innerHTML = (Number(document.getElementById('part-two').innerHTML) +Math.round(00 + 1) );
+    update();
+    }) ;
+document.getElementById('part-three').addEventListener("click", function(){
+    document.getElementById('part-three').innerHTML = (Number(document.getElementById('part-three').innerHTML)+ Math.round(00 +1));
+    update();
+        }) ;
+document.getElementById('part-four').addEventListener("click", function(){
+    document.getElementById('part-four').innerHTML = (Number(document.getElementById('part-four').innerHTML)+Math.round (00 +1));
+    update();
+            })             
+
 
 })();
